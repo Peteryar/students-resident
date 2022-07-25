af6b813cedd5e96e40234c38a873f327214ebe5f
JSVenger <peteryar19@gmail.com>
2022-07-25 20:33:57 +0100

    All functionalities for test passed

--- src/App.js
+++ src/App.js
@@ -4,16 +4,29 @@ import ResidentsList from './Components/ResidentsList';
 import Search from './Components/Search';
 import Error from './Components/Error';
 import 'h8k-components';
+import { useEffect, useState } from 'react';
 
 const title = "Hacker Dormitory";
 function App() {
+  const [residents, setResidents] = useState([]);
+  const [error, setError] = useState('')
+
+  const addResidentStudent = (data)=>{
+     if(data.error){
+       setError(data.error);
+       return
+     }else{
+       setResidents([...residents, data.student])
+     }
+  }
+
   return (
     <div className="App">
         <h8k-navbar header={title}></h8k-navbar>
       <div className="layout-column justify-content-center align-items-center w-50 mx-auto">
-        <Search />
-        <Error/>
-        <ResidentsList/>
+        <Search addResidentStudent={(data)=>addResidentStudent(data)} />
+        {error&&<Error message={error}/>}
+        <ResidentsList residents={residents} />
       </div>
     </div>
   );
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // `joiningDate` && `validityDate` format "yyyy-mm-dd"
+
+import { STUDENTS } from '../studentsList';
 
 function checkValidity(joiningDate, validityDate) {
 	const now = new Date();
@@ -12,20 +14,49 @@ function checkValidity(joiningDate, validityDate) {
 	return (maxValid >= selected) && (maxValid >= today);
 }
 
-function Search() {
+function Search({ addResidentStudent }) {
+
+	const [name, setName] = useState('');
+	const [date, setDate] = useState('');
+
+	const inputChangeHandler = (e) => {
+		switch (e.target.id) {
+			case 'joiningDate':
+				setDate(e.target.value);
+				break;
+			default:
+				setName(e.target.value)
+		}
+	}
+	const handleAddStudent = () => {
+		const studentIndex = STUDENTS.findIndex((student) => student.name.toLocaleLowerCase() === name.toLocaleLowerCase());
+		if (studentIndex === -1) {
+			addResidentStudent({ error: `Sorry, ${name} is not a verified student!` })
+		} else {
+			const validity = checkValidity(date, STUDENTS[studentIndex].validityDate);
+			console.log('validtity', validity);
+			if (!validity) {
+				addResidentStudent({ error: `Sorry, ${name}'s validity has Expired!` })
+			} else {
+				addResidentStudent({ student: STUDENTS[studentIndex].name })
+			}
+		}
+		setDate('');
+		setName('')
+	}
 	return (
 		<div className="my-50 layout-row align-items-end justify-content-end">
 			<label htmlFor="studentName">Student Name:
 				<div>
-					<input id="studentName" data-testid="studentName" type="text" className="mr-30 mt-10"/>
+					<input value={name} onChange={inputChangeHandler} id="studentName" data-testid="studentName" type="text" className="mr-30 mt-10" />
 				</div>
 			</label>
 			<label htmlFor="joiningDate">Joining Date:
 				<div>
-					<input id="joiningDate" data-testid="joiningDate" type="date" className="mr-30 mt-10"/>
+					<input value={date} onChange={inputChangeHandler} id="joiningDate" data-testid="joiningDate" type="date" className="mr-30 mt-10" />
 				</div>
 			</label>
-			<button type="button" data-testid="addBtn" className="small mb-0">Add</button>
+			<button onClick={handleAddStudent} type="button" data-testid="addBtn" className="small mb-0">Add</button>
 		</div>
 	);
 }
